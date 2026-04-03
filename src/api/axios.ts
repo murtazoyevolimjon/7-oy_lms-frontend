2d9b1204d56f05373b1126ca14406baff5786c13
Olimjon Murtazoyev <murtazoyevolimjon734@gmail.com>
2026-04-04 00:13:42 +0500

    .env update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://139.59.33.156:8002/api',
   headers: {
     'Content-Type': 'application/json',
   },
